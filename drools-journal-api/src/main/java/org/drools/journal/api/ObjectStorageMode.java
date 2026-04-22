d8b7f3ab58f4519e44df46d7febcdafdf0c66b5d
Paolo Bizzarri <pibizza@gmail.com>
2026-04-22 12:18:50 +0200

    feat(api): add ObjectStorageStrategy SPI
    Refs #3

---
+++ drools-journal-api/src/main/java/org/drools/journal/api/ObjectStorageMode.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2026 Drools Journal Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.drools.journal.api;
+
+/**
+ * Session-level default for object storage, set via {@code DurableSessionOption}.
+ * A per-object {@link ObjectStorageStrategy} can override this for individual
+ * fact types.
+ */
+public enum ObjectStorageMode {
+    /** Serialize all facts inline in the journal by default. */
+    EMBED,
+    /** Store only reference keys by default; facts live in an external store. */
+    EXTERNAL_REF;
+
+    /** Converts this mode to the equivalent per-object {@link StorageDecision}. */
+    public StorageDecision toDecision() {
+        return this == EMBED ? StorageDecision.EMBED : StorageDecision.EXTERNAL_REF;
+    }
+}
